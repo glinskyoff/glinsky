8284abe94a6510f0dbad9f4360d5a3d6435d7bdb
glinsky off <omsk.den@bk.ru>
2022-03-18 23:06:20 +0600

    glinsky

--- main.py
+++ main.py
@@ -158,7 +158,7 @@ def bot_message(message):
 
             for index, row in enumerate(sort, start = 1):
                 #bot.send_message(message.from_user.id, f"{index})     {row[4]}  |  {row[3]}  | {row[2]} - (@{row[1]})")
-                bot.send_message(message.from_user.id, f"{index})  {row[2]} - (@{row[1]})  |  {row[4]}  |  {row[3]}")
+                bot.send_message(message.from_user.id, f"{index})  {row[2]}  |  {row[4]}  |  {row[3]}")
                 
                 limit = 8
                 if index == limit:
